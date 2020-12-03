6137b7d0fe1926c75511278971bd416bab43c9d8
Salim-Developer <ss13899@ap.be>
2020-12-03 21:53:37 +0100

    Header
    Header met menu items en logo plus styled

---
+++ showroom-agency/src/components/header/Header.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import PropTypes from "prop-types"
+import {HeaderWrapper, Image} from './headerStyles/headerStyles'
+import Menu from './Menu'
+
+const Header = ({ siteTitle }) => {
+  const {logo,wpcontent: {menuItems}} = useStaticQuery(graphql`
+    query{
+      logo: file(relativePath: {eq: "logo_large.png"}){
+        childImageSharp {
+          fixed(quality: 100, width: 200){
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      wpcontent {
+        menuItems {
+          edges {
+            node {
+              label
+              path
+            }
+          }
+        }
+      }
+    }
+    `)
+  return (
+    <HeaderWrapper>
+      <Link to={"/"}>
+        <Image alt="logo computer showroom" fixed={logo.childImageSharp.fixed} />
+      </Link>
+      <Menu menuItems={menuItems.edges} />
+    </HeaderWrapper>
+    )
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
